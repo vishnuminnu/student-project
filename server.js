5183cec7b2fb1815c6f16d20bcdcd9f786714f87
VOLLALA VISHNUVARDHAN <97404199+vishnuminnu@users.noreply.github.com>
2024-03-12 20:47:38 +0530

    updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import morgan from "morgan"
 import {fileURLToPath} from "url"
 import path from "path"
 const app = express()
-app.use(express.static(path.join(__dirname,"./client/build")))
+
 connectDB()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,6 +14,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 //routes
 app.use("/api/v1/auth",authRoutes);
